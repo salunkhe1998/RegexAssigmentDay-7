be3384231d57ce3a7b4c77d263dbabdbd7fcd440
Ashwini <salunkheashwini1998@gmail.com>
2023-03-29 16:18:55 +0530

    Method calling

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using RegexPattern;
+namespace RegexPattern
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regex Pattern Problem!");
+            ValidateRegex.Match();
+        }
+    }
+}
