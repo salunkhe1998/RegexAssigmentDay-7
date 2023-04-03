1528e6356da3d0f4960292d8a14a4c037d9fb98c
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-04-03 20:58:49 +0530

    Update ValidateRegex.cs

--- ValidateRegex.cs
+++ ValidateRegex.cs
@@ -9,26 +9,84 @@ namespace RegexPattern
 {
     public class ValidateRegex
     {
-        public static void Match()
+        public static void Match()      //Q1
         {
             string pattern = "^[a]{1}[b]{2,3}$";
-            string[] inputs = { "ab", "abb", "abbb", "abbbb" };
+            string[] inputs = { "ab","abb","abbb","abbbb"};
             ValidatePattern(inputs, pattern);
         }
-
-        public static void ValidatePattern(string[] inputs, string pattern)
+        public static void FindSequencewithUnderscore()     //Q2
         {
-            foreach (string input in inputs)
+            string pattern = "^[a-z]+_[a-z]+$";
+            string[] inputs = { "ashw_ini", "ashwin_i", "AS_wHini", "A_shwini" };
+            ValidatePattern(inputs, pattern);
+        }
+            public static void ValidatePattern(string[] inputs, string pattern)
+        {
+            foreach(string input in inputs)
             {
-                if (Regex.IsMatch(input, pattern))
+                if( Regex.IsMatch(input, pattern) )
                 {
-                    Console.WriteLine("{0} is valid.", input);
+                    Console.WriteLine("{0} is valid.",input);
                 }
                 else
                 {
                     Console.WriteLine("{0} is invalid.", input);
                 }
             }
+        }
+
+        public static void ValidateHTMLTags(string inputString)     //Q3
+        {
+            Regex regex = new Regex(@"<[^>]+>");
+
+            MatchCollection matches = regex.Matches(inputString);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+
+        public static void CountOccurence(string inputString)       //Q4
+        {
+            Regex regex = new Regex(@"fox(es)?");
+
+            MatchCollection matches = regex.Matches(inputString);
+            Console.WriteLine("The Count of given Pattern in given String " + matches.Count);
+
+        }
+
+        public static void FileExtension()      //Q5
+        {
+            Regex regex = new Regex(@"([^\s]+(\.(?i)(jpe?g|png|gif|bmp))$)");
+
+            Console.WriteLine("Enter filename with image file extension : ");
+            string filename = Console.ReadLine();
+            if (regex.IsMatch(filename))
+            {
+                Console.WriteLine("Valid file name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid file name");
+            }
+        }
+
+        public static void WebsiteAddress()     //Q6
+        {
+            Regex regex = new Regex(@"^(http|http(s)?://)?([\w-]+\.)+[\w-]+[.com|.in|.org]+(\[\?%&=]*)?");
+            Console.WriteLine("Enter a url ");
+            string Website = Console.ReadLine();
+            if (regex.IsMatch(Website))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Not valid");
+            }
+
         }
     }
 }
